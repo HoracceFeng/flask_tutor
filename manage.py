8b57fa95deea51034a7fccdc14891cad55eb01be
horacce <haonanfengus@outlook.com>
2017-10-11 17:01:01 +0800

    index.html still not work, need to use jinja2 if continue

--- manage.py
+++ manage.py
@@ -1,1 +1,18 @@
 ## management script -- for database interactive and server start ##
+
+# -*- conding: utf-8 -*-
+
+from flask_script import Manager, Server
+from app import app
+
+manager = Manager(app)
+
+manager.add_command("runserver",
+	Server(host='0.0.0.0', port=5000, use_debugger=True))
+
+
+if __name__ == '__main__':
+	manager.run()
+
+
+
